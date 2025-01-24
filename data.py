915672b901ef5b4b9f2cd5f49ca9a081ba105549
Dennis Noll <dennis.noll@cern.ch>
2025-01-23 19:24:58 -0800

    [dev] move code in respective modules

---
+++ data.py
@@ -0,0 +1,37 @@
+import numpy as np
+
+def create_data_gaussian(
+    n_total,
+    neg_frac=0.1,
+    neg_weight=1,
+    pos_loc=0,
+    pos_scale=1,
+    neg_loc=0,
+    neg_scale=0.5,
+):
+    """
+    n_total: total number of samples
+    neg_frac: fraction of (unweighted) negative samples
+    neg_weight: weight of negative samples
+    """
+
+    n_neg = int(n_total * neg_frac)
+    n_pos = n_total - n_neg
+
+    pos = np.random.normal(loc=pos_loc, scale=pos_scale, size=n_pos)
+    neg = np.random.normal(loc=neg_loc, scale=neg_scale, size=n_neg)
+
+    pos_weights = np.ones(n_pos)
+    neg_weights = -np.ones(n_neg) * neg_weight
+    
+    # pos_weights = np.random.normal(loc=2, scale=0.3, size=n_pos)
+    # neg_weights = np.random.normal(loc=-1, scale=0.2, size=n_neg)
+
+    return pos, neg, pos_weights, neg_weights
+
+
+def prepare_data_natural(pos, neg, pos_weights, neg_weights):
+    x = np.concatenate([pos, neg])
+    y = np.concatenate([np.ones_like(pos_weights), np.zeros_like(neg_weights)])
+    w = np.concatenate([pos_weights, neg_weights])
+    return x, y, w
---
+++ model.py
@@ -0,0 +1,81 @@
+import numpy as np
+import tensorflow as tf
+from sklearn.model_selection import train_test_split
+
+from data import prepare_data_natural
+
+def prepare_data_resampler(pos, neg, pos_weights, neg_weights):
+    tot = np.concatenate([pos, neg])
+    tot_weights = np.concatenate([pos_weights, neg_weights])
+
+    x = np.concatenate([tot, tot])
+    y = np.concatenate([np.ones_like(tot_weights), np.zeros_like(tot_weights)])
+    w = np.concatenate([tot_weights, np.ones_like(tot_weights)])
+    return x, y, w
+
+
+def prepare_data_reweighter(pos, neg, pos_weights, neg_weights):
+    x = np.concatenate([pos, neg])
+    y = np.concatenate([np.ones_like(pos_weights), np.zeros_like(neg_weights)])
+    w = np.concatenate([pos_weights, -neg_weights])
+    return x, y, w
+
+
+def split(
+    *arrays,
+    test_size=0.5,
+    random_state=42,
+    **kwargs,
+):
+    return train_test_split(
+        *arrays,
+        test_size=test_size,
+        random_state=random_state,
+        **kwargs,
+    )
+
+
+def get_train(*data, **kwargs):
+    x_train, _, y_train, __, w_train, ___ = split(*data, **kwargs)
+    return x_train, y_train, w_train
+
+
+def get_val(*data, **kwargs):
+    _, x_val, __, y_val, ___, w_val = split(*data, **kwargs)
+    return x_val, y_val, w_val
+
+
+def simple_model(input_shape=(1,)):
+    return tf.keras.Sequential(
+        layers=[
+            tf.keras.Input(shape=input_shape),
+            tf.keras.layers.Dense(128, activation="relu"),
+            tf.keras.layers.Dense(128, activation="relu"),
+            tf.keras.layers.Dense(1, activation="sigmoid"),
+        ]
+    )
+
+
+def apply_reweighter(*data, reweighter):
+    x, y, w = prepare_data_reweighter(*data)
+    pred = reweighter.predict(x, batch_size=10_000)[:, 0]
+    ratio = 1 / pred - 1
+    w_new = w * (1 - ratio) / (1 + ratio)
+    return x, y, w_new
+
+
+# def check_reweighter(vals, reweighter):
+#     # Analytic check of reweighter ratio
+#     # vals = (bins[1:] + bins[:-1]) / 2
+#     ratio = 1 / reweighter.predict(vals) - 1
+
+#     plt.plot(vals, ratio, label="DNN Ratio")
+#     plt.plot(vals, np.abs(neg)/pos, label="Analytic Ratio")
+#     plt.legend()
+
+
+def apply_resampler(*data, resampler):
+    x, y, _ = prepare_data_natural(*data)
+    pred = resampler.predict(x, batch_size=10_000)[:, 0]
+    w_new = pred / (1.0 - pred)
+    return x, y, w_new
